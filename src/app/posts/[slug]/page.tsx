2022ed6e04cee5ff22fcdb6823501982b49541df
bekbohato <riannazar03@gmail.com>
2024-11-18 13:42:25 +0700

    first commit

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,31 @@
+import Mdx from '@/components/mdx-components';
+import { allPosts } from 'contentlayer/generated';
+import { format, parseISO } from 'date-fns';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { getMDXComponent } from 'next-contentlayer2/hooks';
+ 
+interface PostPageProps {
+  params: {
+    slug: string;
+  };
+}
+ 
+export default function PostPage({ params }: PostPageProps) {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+ 
+  if (!post?.body.code) {
+    return <div>Post not found</div>;
+  }
+ 
+  return (
+    <article className='py-8 mx-auto max-w-xl'>
+      <div className='mb-8 text-center'>
+        <time dateTime={post.date}>
+          {format(parseISO(post.date), 'LLLL d, yyyy')}
+        </time>
+        <h1>{post.title}</h1>
+      </div>
+      <Mdx code={post.body.code} />
+    </article>
+  );
+}
